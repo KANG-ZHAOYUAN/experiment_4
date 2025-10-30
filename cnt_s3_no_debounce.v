2c5311e17185e2e5606d45574bbc50e39d4000af
KANG-ZHAOYUAN <kangzhaoyuan@163.com>
2025-10-30 22:32:05 +0800

    °ÑһЩһλ¸ĳÉÁËËÄλ£¬Õâ¸ü¼ÓÑϽ÷Áˡ£

--- cnt_s3_no_debounce.v
+++ cnt_s3_no_debounce.v
@@ -18,24 +18,25 @@ always @(posedge clk or posedge rst) begin
 end
 
 wire key_s3_rise;  // S3上升沿（当前1，上一周期0）
-assign key_s3_rise = key_s3 & ~prev_key_s3;
+assign key_s3_rise = key_s3 & ~prev_key_s3;// 上升沿检测
 
 // -------------------------- 2. 8位计数（00~99循环） --------------------------
 // 高4位：十位（0~9），低4位：个位（0~9）
 always @(posedge clk or posedge rst) begin
     if (rst) begin
-        cnt_out <= 8'h00;  // 复位清零
-    end else if (key_s3_rise == 1'b1) begin  // 上升沿触发计数
-        if (cnt_out[3:0] == 4'h9) begin  // 个位满9，个位清零，十位加1
+        cnt_out <= 8'h00;                       // 复位清零,count_out直接按照十进制的逻辑来进行计数
+    end else if (key_s3_rise == 1'b1) begin     // 上升沿触发计数
+        if (cnt_out[3:0] == 4'h9) begin         // 个位满9，个位清零，十位加1
             cnt_out[3:0] <= 4'h0;
-            if (cnt_out[7:4] == 4'h9) begin  // 十位满9，整体清零（00~99循环）
+            if (cnt_out[7:4] == 4'h9) begin     // 十位满9，整体清零（00~99循环），显然，在个位满9的情况下，十位才可能满9会清零
                 cnt_out[7:4] <= 4'h0;
             end else begin
-                cnt_out[7:4] <= cnt_out[7:4] + 1'b1;
+                cnt_out[7:4] <= cnt_out[7:4] + 4'b1; //进位操作
             end
         end else begin  // 个位未满9，个位加1
-            cnt_out[3:0] <= cnt_out[3:0] + 1'b1;
+            cnt_out[3:0] <= cnt_out[3:0] + 4'b1; 
         end
+
     end else begin
         cnt_out <= cnt_out;  // 无触发，保持计数
     end
